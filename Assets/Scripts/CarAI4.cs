3b02e1cfe90c0f362ddf31cb22a6e1b852aa665c
Gnabriel <elleb@kth.se>
2021-02-24 16:23:30 +0100

    Added linear acceleration

--- Assets/Scripts/CarAI4.cs
+++ Assets/Scripts/CarAI4.cs
@@ -20,6 +20,7 @@ namespace UnityStandardAssets.Vehicles.Car
         public float x;
         public float z;
         public int current_agent;
+        public float max_dist;
 
         private void Start()
         {
@@ -58,55 +59,58 @@ namespace UnityStandardAssets.Vehicles.Car
                 }
             }
 
-            Debug.Log("TEST");
-
-            // Plan your path here
-            // ...
+            // Calculate length of map diagonal.
+            Vector3 top_left = new Vector3(terrain_manager.myInfo.x_low, 0.0f, terrain_manager.myInfo.z_low);
+            Vector3 bottom_right = new Vector3(terrain_manager.myInfo.x_high, 0.0f, terrain_manager.myInfo.z_high);
+            max_dist = Vector3.Distance(top_left, bottom_right);
         }
 
 
         private void FixedUpdate()
         {
-
-
             // Execute your path here
             GameObject lead_car = friends[0];
             GameObject current_car = m_Car.gameObject;
+            Vector3 current_position = current_car.transform.position;
+            Vector3 lead_position = lead_car.transform.position;
 
             Agent agent = agents[current_agent];
             float current_angle = lead_car.transform.rotation.y;
             float rotated_x = agent.wanted_x * Mathf.Cos(current_angle) + agent.wanted_z * Mathf.Sin(current_angle);
             float rotated_z = agent.wanted_z * Mathf.Cos(current_angle) - agent.wanted_x * Mathf.Sin(current_angle);
-            Vector3 wanted_position = new Vector3(lead_car.transform.position.x + rotated_x,
-                lead_car.transform.position.y, lead_car.transform.position.z + rotated_z);
-            Debug.DrawLine(lead_car.transform.position, wanted_position, Color.red);
+            Vector3 wanted_position = new Vector3(lead_position.x + rotated_x, lead_position.y, lead_position.z + rotated_z);
+            Debug.DrawLine(lead_position, wanted_position, Color.red);
 
-            Vector3 direction = (wanted_position - current_car.transform.position).normalized;
+            Vector3 direction = (wanted_position - current_position).normalized;
             bool is_to_the_right = Vector3.Dot(direction, transform.right) > 0f;
             bool is_to_the_front = Vector3.Dot(direction, transform.forward) > 0f;
 
             float steering = 0f;
             float acceleration = 0;
 
+            Vector3 target_position = agent.GetTargetPosition(lead_position);
+
+            Debug.Log("Acceleration: " + agent.GetAcceleration(max_dist, current_position, target_position));
+
             if (is_to_the_right && is_to_the_front)
             {
-                steering = 1f;
-                acceleration = 0.75f;
+                steering = 0.5f;
+                acceleration = agent.GetAcceleration(max_dist, current_position, target_position);
             }
             else if (is_to_the_right && !is_to_the_front)
             {
-                steering = -1f;
-                acceleration = -0.75f;
+                steering = -0.5f;
+                acceleration = -agent.GetAcceleration(max_dist, current_position, target_position);
             }
             else if (!is_to_the_right && is_to_the_front)
             {
-                steering = -1f;
-                acceleration = 0.75f;
+                steering = -0.5f;
+                acceleration = agent.GetAcceleration(max_dist, current_position, target_position);
             }
             else if (!is_to_the_right && !is_to_the_front)
             {
-                steering = 1f;
-                acceleration = -0.75f;
+                steering = 0.5f;
+                acceleration = -agent.GetAcceleration(max_dist, current_position, target_position);
             }
 
             m_Car.Move(steering, acceleration, acceleration, 0f);
@@ -160,8 +164,6 @@ namespace UnityStandardAssets.Vehicles.Car
             Debug.Log("Steering:" + steering + " Acceleration:" + acceleration);
             m_Car.Move(steering, acceleration, acceleration, 0f);
             //m_Car.Move(0f, -1f, 1f, 0f);*/
-
-
         }
     }
 
@@ -172,8 +174,24 @@ namespace UnityStandardAssets.Vehicles.Car
 
         public Agent(float w_x, float w_z)
         {
-            wanted_x = w_x;
-            wanted_z = w_z;
+            this.wanted_x = w_x;
+            this.wanted_z = w_z;
+        }
+
+        public Vector3 GetTargetPosition(Vector3 lead_position)
+        {
+            return new Vector3(lead_position[0] + wanted_x, 0.0f, lead_position[2] + wanted_z);
+        }
+
+        public float GetAcceleration(float max_dist, Vector3 current_position, Vector3 target_position)
+        {
+            // Linear acceleration based on distance to target position.
+            float speed_factor = 10.0f;
+            float min_acceleration = 0.1f;
+
+            float travel_dist = Vector3.Distance(current_position, target_position);
+            float acceleration = Math.Max(min_acceleration, speed_factor * (travel_dist / max_dist));
+            return acceleration;
         }
     }
 }
